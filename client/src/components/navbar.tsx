5eefdbf3e72769bceb7098366720278018a5c352
Prasad Lakshan <kpdplakshan@gmail.com>
2023-08-15 06:06:32 +0530

    sty: navbar styles done

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,7 +1,11 @@
+import { Button } from 'react-bulma-components';
+import { BiHomeAlt } from 'react-icons/bi';
+import { FaRegUser } from 'react-icons/fa';
 import { FiSearch } from 'react-icons/fi';
+
 const Navbar = () => {
   return (
-    <nav className="navbar ml-6 my-3" role="navigation" aria-label="main navigation">
+    <nav className="navbar mx-6 my-3" role="navigation" aria-label="main navigation">
       <div className="navbar-brand ml-4 navbar-item">
         <img
           src="https://consumer-component-library.roocdn.com/26.21.0/static/images/logo-teal.svg"
@@ -14,17 +18,23 @@ const Navbar = () => {
           <span className="icon is-small is-right">
             <FiSearch size={18} />
           </span>
-          <input className="input" type="search" placeholder="Search..." />
+          <input
+            className="input has-text-weight-semibold"
+            type="search"
+            placeholder="Search Tosted St-Martin's Lane"
+          />
         </p>
       </div>
 
       <div className="navbar-end">
-        <a className="navbar-item is-primary" href="#">
-          Button 1
-        </a>
-        <a className="navbar-item" href="#">
-          Button 2
-        </a>
+        <Button className="primary mr-3">
+          <BiHomeAlt className="mr-2 primary-icons" />
+          Sign up Or Login
+        </Button>
+        <Button>
+          <FaRegUser className="mr-2 primary-icons" />
+          Account
+        </Button>
       </div>
     </nav>
   );
